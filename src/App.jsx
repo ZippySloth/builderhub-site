c1182ad559d7d502b9714c5cc9266c9207b1bfdf
Rhita <Rhita@frs-macbook-pro.home>
2025-04-10 12:54:27 -0400

    modified routing in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import AboutPage from './Pages/AboutPage';
 import ServicesPage from './Pages/ServicesPage';
 import HomePage from './Pages/HomePage';
 import Joinus from './Components/Joinus/Joinus';
-import RedditDatabase from './Pages/RedditDatabase'; // Ensure this import is present
+import RedditDatabasePage from './Pages/RedditDatabase'; // Ensure this import is present
 import ScrollToTop from './ScrollToTop';
 import CookiesConsent from "@/Components/CookiesConsent/CookiesConsent";
 import { AnalyticsScripts } from "./Components/AnalyticsScripts"
@@ -39,7 +39,7 @@ const App = () => {
             />
             <Route path="/web" element={<ServicesPage />} />
             <Route path="/about" element={<AboutPage />} />
-            <Route path="/viral" element={<RedditDatabase />} />
+            <Route path="/viral" element={<RedditDatabasePage />} />
             <Route path="/contact" element={<ContactPage />} />
             <Route path="/privacypolicy" element={<PrivacyPolicy />} />
           </Routes>
